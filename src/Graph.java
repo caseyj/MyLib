5f209cb565606beef3e7cef3df3d7ab573bc76c8
JohnBCasey <caseyjohnb@gmail.com>
2016-01-04 23:34:12 -0500

    Implemented Edmonds-Karp algorithm for network flow tested and generates correct amount of flow for a given network. Commented completely in order to facilitate best results with a given user who wishes to read. Proposed next version contains the print statements and verbose moding for the edmonds-karp algorithm. This paired with more algorithms and possibly a data structure just for edges for minimum spanning trees

--- src/Graph.java
+++ src/Graph.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
  * @author Casey
  *
  */
+//import java.util.Scanner;
 
 /**
  * A doubly linked list implementation for graph objects
@@ -257,6 +258,8 @@ class gVert{
 		a.CAPACITY[b.NAME] = cap;
 		a.MLIST++;
 		b.FlowFrom[a.NAME] = a;
+		//the reverse edge is set here, with this the reverse edge is always
+		//providing flow back such that forward edge - reverse edge = 0
 		b.FLOWIN[a.NAME] = cap;
 		b.FLOWINLIST++;
 	}
@@ -315,50 +318,92 @@ public class Graph {
 	}
 	
 	/**
-	 * !!!!!!Must test this!!!!!
-	 * @param start
-	 * @param target
-	 * @return
+	 * THIS OPERATION WILL ALTER THE GRAPH PERMANENTLY!!!
+	 * Runs the Edmonds Karp algorithm for network flow
+	 * in order to calculate both the maximum flow, as well
+	 * as produce the proper network graph to display
+	 * maximum flow
+	 * 
+	 * additions to be added:
+	 * 	Verbose mode: print every modification of the graph
+	 * 		this would constantly print or generate
+	 * 		the current 'augmented path'. This is not for
+	 * 		any deployment but would be for creating GUI's
+	 * 		for demonstrating how this algorithm finds it's pathing
+	 * 
+	 * @param start the source of flow in the network, we 
+	 * 		do assume this is at this.VERTICES[0]
+	 * @param target the sink/terminal of flow in the
+	 * 		network, we do assume this is at 
+	 * 		this.VERTICES[this.size - 1]
+	 * @return the maximum flow of the graph
 	 */
 	public int EKflow(gVert start, gVert target){
+		//initial flow is 0
 		int flow = 0;
+		//a bfs runs every iteration until no augmenting path can be found
 		while(true){
-			// GCWOK approved!
 			gQueue queue = new gQueue();
 			queue.enqueue(start);
 			//parent also used to identify all seen nodes
 			int[] parent = new int[this.SIZE];
 			Arrays.fill(parent, -1);
+			//does not allow the start to be found again!
+			parent[start.NAME] = start.NAME;
 			while(queue.size>0){
+				//operate with top of the queue
 				gVert current = queue.dequeue();
+				//check each neighbor
 				for(int i = 0; i<current.MLIST; i++){
-					if(parent[current.NEIGHBORS[i].NAME]!=-1 && current.CAPACITY[i]>current.COST[i]){
+					//if the node has already set a parent, it has been seen
+					//do not check nodes that have already been seen or 
+					//	have a maximum capacity flowing
+					if(parent[current.NEIGHBORS[i].NAME]==-1 && current.CAPACITY[current.NEIGHBORS[i].NAME]>current.COST[current.NEIGHBORS[i].NAME]){
 						parent[current.NEIGHBORS[i].NAME]= current.NAME;
 						queue.enqueue(current.NEIGHBORS[i]);
 					}
 				}
 			}
+			//if we cannot create a path from start to target
+			//		we have the maximum flow
 			if(parent[target.NAME]==-1){
-				break;
+				return flow;
 			}
+			//otherwise calculate maximum flow
 			else{
+				//we always assume the first and final vertices are start and
+				//		sink respectively
+				//set the largest difference in flow equal to max
 				int df = Integer.MAX_VALUE;
-				for(int i = parent.length-1; i>=0; i = parent[i]){
+				//run a for loop backtracking the fastest path to the sink
+				for(int i = parent.length-1; i>0; i = parent[i]){
 						//the vertex that pumps flow into this vertex
-						//gVert current = this.VERTICES[i];
+						//set in as the current vertex going to the current node
 						gVert in = this.VERTICES[parent[i]];
+						//set df to the min between df and the cost between the vertices
 						df = Math.min(df, in.CAPACITY[i]-in.COST[i]);
+						if(i==0){
+							break;
+						}
 				}
+				//we now have the minimum flow, add it to the flow value
+				flow+=df;
+				//backtrack through the fastest path updating the flow at every
+				//		forward and reverse edge
 				for(int i = parent.length-1; i>=0; i = parent[i]){
+					//get the current vertex
 					gVert current = this.VERTICES[i];
+					//get that vertex's parent
 					gVert in = this.VERTICES[parent[i]];
+					//update current flows, this mutates the graph permanently!
 					in.COST[current.NAME] += df;
 					current.FLOWIN[in.NAME]-=df;
+					if(i==0){
+						break;
+					}
 				}
 			}
 		}
-		
-		return flow;
 	}
 	
 	/**
@@ -384,5 +429,96 @@ public class Graph {
 		
 	}
 	
+	public static void main(String[] args){
+		/**
+		 * 
+		 * 
+		 * Below have been used for testing the network flow 
+		 * a more conducive driver will be written to allow 
+		 * user defined graphs for particular problem sets utilizing graphs
+		 * that users may be interested in utilizing as well as a proper print
+		 * method.
+		 */
+		
+		
+/*		
+		Scanner s = new Scanner(System.in);
+		while(!s.hasNextInt())s.next();
+		int kids = s.nextInt();
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int c = s.nextInt();
+		
+		Graph[] bpm = new Graph[3];
+		for(int i = 0; i<3; i++){
+			int gSize;
+			gVert start, terminal;
+			switch(i){
+				case(0):
+					gSize = a+kids+2;
+					start = new gVert(0, gSize);
+					terminal = new gVert((gSize-1), gSize);
+					bpm[i] = new Graph(gSize);
+					bpm[i].VERTICES[0] = start;
+					bpm[i].VERTICES[gSize-1] = terminal;
+					break;
+				case(1):
+					gSize = b+kids+2;
+					start = new gVert(0, gSize);
+					terminal = new gVert((gSize-1), gSize);
+					bpm[i] = new Graph(gSize);
+					bpm[i].VERTICES[0] = start;
+					bpm[i].VERTICES[gSize-1] = terminal;
+					break;
+				case(2):
+					gSize = c+kids+2;
+					start = new gVert(0, gSize);
+					terminal = new gVert((gSize-1), gSize);
+					bpm[i] = new Graph(gSize);
+					bpm[i].VERTICES[0] = start;
+					bpm[i].VERTICES[gSize-1] = terminal;
+					break;
+			}
+		}
+		for(int j = 0; j<3; j++){	
+			for(int i = 1; i<bpm[j].SIZE-1; i++){
+				gVert start = bpm[j].VERTICES[0];
+				gVert fin = bpm[j].VERTICES[bpm[j].SIZE-1];
+				bpm[j].VERTICES[i] = new gVert(i, bpm[j].SIZE);
+				if(i<=kids){
+					start.add_Direct_FLOW_Neighbor_Costly(start, bpm[j].VERTICES[i], 1);
+				}
+				else{
+					fin.add_Direct_FLOW_Neighbor_Costly(bpm[j].VERTICES[i], fin, 1);
+				}
+			}
+		}
+	
+		int kidCounter = 1;
+		int objCounter = 0;
+		while (kidCounter<=kids){
+			int curr = s.nextInt();
+			if(curr!=0){
+				gVert kid = bpm[objCounter].VERTICES[kidCounter];
+				kid.add_Direct_FLOW_Neighbor_Costly(kid, bpm[objCounter].VERTICES[curr+kids], 1);
+				}
+			else{
+				objCounter++;
+				if(objCounter==3){
+					objCounter=0;
+					kidCounter++;
+				}
+			}
+			
+		}
+		int flow = bpm[0].EKflow(bpm[0].VERTICES[0], bpm[0].VERTICES[bpm[0].SIZE-1]);
+		System.out.println(flow);
+			
+		s.close();
+*/			
+			
+			
+	}
+			
 	
 }
